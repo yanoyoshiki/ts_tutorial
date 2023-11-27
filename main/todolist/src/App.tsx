52bfe2b882a45602b9b98b79c98587c5349c8e91
yanoyoshiki <yanoyoshikisince2022@gmail.com>
2023-11-27 16:39:50 +0900

    can edit todolist

--- main/todolist/src/App.tsx
+++ main/todolist/src/App.tsx
@@ -2,24 +2,64 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setinputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [todos, setTodos] = useState<Todo[]>([])
   type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
 
-    inputValue: String
-    id: Number
-    checked: Boolean
-  }
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setinputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // make new todo list
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setinputValue("");
+  };
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const NewTodos = todos.map((todo) => {
+      if (todo.id == id){
+        todo.inputValue = inputValue
+      };
+      return todo;
+    });
+    setTodos(NewTodos);
+  };
+  
   return (
     <div className="App">
       <div>
-        <h2>
-          TODO list with Typescript
-          <form onSubmit={() => {}}>
-            <input type="text" onChange={() => {}} className='inputText'/>
-            <input type="submit" value="Submit" className='submitButton' />
-          </form>
-        </h2>
+        <h2>TODO list with Typescript</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input type="text" onChange={(e) => handleChange(e)} className='inputText'/>
+          <input type="submit" value="Submit" className='submitButton' />
+        </form>
+        <ul className='todoList'>
+          {todos.map((todo) => (
+            <li key = {todo.id}>
+              <input 
+              type="text" 
+              onChange={(e) => handleEdit(todo.id, e.target.value)} 
+              className='inputText'
+              value={todo.inputValue}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
